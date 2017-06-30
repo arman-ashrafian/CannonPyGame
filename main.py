de18bfaf2781821d01b203184eed9ce21dd86afc
Arman Ashrafian <ashy63@gmail.com>
2017-06-29 19:01:10 -0700

    gitignore

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from game import runGame, hitArray
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+# from game import runGame, hitArray
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
 
-runGame()
+# runGame()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
-ax.set_xlabel('X Position')
-ax.set_ylabel('Y Position')
-ax.set_zlabel('Angle')
+# ax.set_xlabel('X Position')
+# ax.set_ylabel('Y Position')
+# ax.set_zlabel('Angle')
 
-ax.scatter(hitArray[0], hitArray[1], hitArray[2], c='r', marker='o')
-plt.show()
+# ax.scatter(hitArray[0], hitArray[1], hitArray[2], c='r', marker='o')
+# plt.show()
